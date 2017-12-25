38a99c66a8063d6adf4551bddabb968bc15e5d84
shawn button <shawnbutton@gmail.com>
2017-12-25 13:44:53 -0500

    added call to github

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -5,9 +5,13 @@ defmodule Issues.CLI do
   the various functions that end up generating a
   table of the last _n_ issues in a github project
   """
+
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
+
   @doc """
   `argv` can be -h or --help, which returns :help.
   Otherwise it is a github user name, project name, and (optionally)
@@ -38,4 +42,16 @@ defmodule Issues.CLI do
       _ -> :help
     end
   end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
+
 end
